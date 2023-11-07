832b5e3582377ffb1c48f392b445751df65a8db4
adam <adam.guo@mail.goldennet.com.tw>
2023-11-07 13:44:02 +0800

    test-3

--- src/pages/user/Registered.jsx
+++ src/pages/user/Registered.jsx
@@ -4,14 +4,10 @@ import { Row, Col, Card, Button, Dropdown } from "antd";
 const items = [
   {
     key: "1",
-    label: "linePay",
-  },
-  {
-    key: "2",
     label: "全支付",
   },
   {
-    key: "3",
+    key: "2",
     label: "綠界",
   },
 ];
@@ -23,12 +19,12 @@ export default function Registered() {
       {
         templateName: "寵物展",
         status: "1",
-        pay: ["linePay", "全支付", "綠界"],
+        pay: ["全支付", "綠界"],
       },
       {
         templateName: "飲料展",
         status: "0",
-        pay: ["linePay", "全支付", "綠界"],
+        pay: ["全支付", "綠界"],
       },
     ];
     setIsShowRelease(fakeData);
